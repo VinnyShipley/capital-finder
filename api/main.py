e68ce059132a3bc50726aec39f71edcfd74af810
Vinny <vinnyshipley@gmail.com>
2022-08-08 21:50:00 -0700

    file names

--- api/main.py
+++ api/main.py
@@ -7,15 +7,22 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-
     
+    # grabs everything past .app in the url
     url_components = parse.urlsplit(self.path)
+    # parses into tuple
     query_string_list = parse.parse_qsl(url_components.query)
+    #turns tuple into dictionary so it can be accessed with key value pairs
     dict_query_str = dict(query_string_list)
     
+    
+    #separating the queries into variables 
     country = dict_query_str['country']
     capital = dict_query_str['capital']
+    
+    # output message
     message = f'the capital of {country} is {capital}'
+    #prints the message
     self.wfile.write(message.encode())
     return
   
