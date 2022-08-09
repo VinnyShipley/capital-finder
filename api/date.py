8eac711a780149c14a1ac49294ac4d608cb829c1
Vinny <vinnyshipley@gmail.com>
2022-08-08 21:33:57 -0700

    untupling

--- api/date.py
+++ api/date.py
@@ -11,8 +11,9 @@ class handler(BaseHTTPRequestHandler):
     
     url_components = parse.urlsplit(self.path)
     query_string_list = parse.parse_qsl(url_components.query)
+    dict_query_str = dict(query_string_list)
     
-    message = str(query_string_list)
+    message = str(dict_query_str)
     #message = f'the capital of {country} is {capital}'
     self.wfile.write(message.encode())
     return
