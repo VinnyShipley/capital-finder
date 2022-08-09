325cb0cb5007f425372b3942e181fd645742c406
Vinny <vinnyshipley@gmail.com>
2022-08-08 21:31:43 -0700

    multi iso

--- api/date.py
+++ api/date.py
@@ -10,8 +10,9 @@ class handler(BaseHTTPRequestHandler):
 
     
     url_components = parse.urlsplit(self.path)
+    query_string_list = parse.parse_qsl(url_components.query)
     
-    message = str(url_components.query)
+    message = str(query_string_list)
     #message = f'the capital of {country} is {capital}'
     self.wfile.write(message.encode())
     return
